c6c5eebb16bb656800744da881a853866985a1df
Alfredo Arriola Jr <73959568+alfredoarriolajr@users.noreply.github.com>
2022-12-28 01:06:05 -0800

    remove loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,28 @@
-import "../styles/globals.css";
+import '../styles/globals.css';
 
 //React imports
-import { useEffect, useState } from "react";
-import ClipLoader from "react-spinners/ClipLoader";
+import { useEffect, useState } from 'react';
 
 //Near imports
-import { initNear } from "../near/near-setup";
+import { initNear } from '../near/near-setup';
 
 function MyApp({ Component, pageProps }) {
-  const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
 
-  //Loading the NEAR API and setting up the wallet and contract
-  //At the start of the app
-  useEffect(() => {
-    initNear();
-    setIsLoading(false);
-  }, []);
+    //Loading the NEAR API and setting up the wallet and contract
+    //At the start of the app
+    useEffect(() => {
+        initNear();
+        setIsLoading(false);
+    }, []);
 
-  return isLoading ? (
-    <div className="center-div">
-      <ClipLoader color={"#000"} loading={true} size={50} />
-    </div>
-  ) : (
-    <Component {...pageProps} />
-  );
+    return isLoading ? (
+        <div className='center-div'>
+            <p>loading</p>
+        </div>
+    ) : (
+        <Component {...pageProps} />
+    );
 }
 
 export default MyApp;
